7265db6ab65afb9d7d61cf78db862ee5e4ff3c3b
Barbie <barbara.kelly892@gmail.com>
2019-11-09 13:36:07 -0500

    Got FourSquare API to work

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ var exploreButton = document.getElementById("explore");
 var container = document.getElementById("container");
 var fiveRestaurants = [];
 
-var googleAPIKey = "AIzaSyA_GPOFfKyGMHgDf7NT1V9_OOVAy-Z1hgM";
-var input = "Atlanta Tech Village";
+    const clientId = "YRYBPL3XX1AQMOAQF3EBE1Z2MWBGYWMH3F5U4XQKWWJ2ZUO3";// Required
+    const clientSecret = "DAHBINRS4RAPF55R30CJUOXLHHRHS3XISOAI1C35BXAP4IIF";//Required
+    var endPoints = {
+//         'getVenueDetailById': 'https://api.foursquare.com/v2/venues/',
+//         'getVenueCategories': 'https://api.foursquare.com/v2/venues/categories',
+            'getPopularVenues': 'https://api.foursquare.com/v2/venues/explore',
+//         'searchVenues': 'https://api.foursquare.com/v2/venues/search',
+//         'getTrendingVenues': 'https://api.foursquare.com/v2/venues/trending',
+    }
 
+    
+    var getVenueByLocation = function () {
+        axios.get(`${endPoints.getPopularVenues}?client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee`)
+            .then(function(data) {
+                console.log(data)
+                // Code for handling API response
+            })
+            .catch(function(error) {
+                console.log(error)
+                // Code for handling errors
+        });
+    }
 
-axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${googleAPIKey}&input=${input}&inputtype=textquery`)
-.then((res) => {
-  console.log(res)
- })
- .catch((err) => {
-  console.log ('error')
- })
-
- 
+    getVenueByLocation();
