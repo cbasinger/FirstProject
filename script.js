058448a6a1496bdfe3c91e44f6454a7c3072be42
Chelsea Basinger <Chelsea@Zacbook.local>
2019-11-12 21:27:47 -0500

    different category pulls from API

--- script.js
+++ script.js
@@ -17,11 +17,28 @@ var fiveRestaurants = [];
     }
 function inputFunction() {
     var zipInput= document.getElementById("zipcodeInput").value;
-    axios.get(`https://api.foursquare.com/v2/venues/search?near=${zipInput}&client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=5`)
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-            // Code for handling errors
-    }
+    
+    //bars    
+    axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d116941735&near=${zipInput}&client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=5`)
+    .then (data => console.log(data))
+    .catch(err => console.log(err))  
+
+    //restaurants
+    axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&near=${zipInput}&client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=5`)        
+    .then (data => console.log(data))
+    .catch(err => console.log(err))    
+
+    //parks
+    axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d163941735&near=${zipInput}&client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=5`)
+    .then (data => console.log(data))
+    .catch(err => console.log(err))   
+    
+    //events
+    axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06373d81259&near=${zipInput}&client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=5`)
+    .then (data => console.log(data))
+    .catch(err => console.log(err))  
+
+}
 
     
     
