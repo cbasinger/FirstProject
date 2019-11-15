427b9021357cd9cd351cbb9f9ca4b406558ef681
Barbie <barbara.kelly892@gmail.com>
2019-11-14 20:41:21 -0500

    API call is working. Have a functioning array of five restaurant objects

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 var exploreButton = document.getElementById("explore");
+const fourSquareClientId = "YRYBPL3XX1AQMOAQF3EBE1Z2MWBGYWMH3F5U4XQKWWJ2ZUO3";// Required
+const fourSquareClientSecret = "DAHBINRS4RAPF55R30CJUOXLHHRHS3XISOAI1C35BXAP4IIF";//Required
+var locationSearchElement = document.getElementById("locationSearch");
 var container = document.getElementById("container");
+var currentLocation = "33.853510, -84.368667";
 var fiveRestaurants = [];
 
-    const clientId = "YRYBPL3XX1AQMOAQF3EBE1Z2MWBGYWMH3F5U4XQKWWJ2ZUO3";// Required
-    const clientSecret = "DAHBINRS4RAPF55R30CJUOXLHHRHS3XISOAI1C35BXAP4IIF";//Required
-    var endPoints = {
-//         'getVenueDetailById': 'https://api.foursquare.com/v2/venues/',
-//         'getVenueCategories': 'https://api.foursquare.com/v2/venues/categories',
-            'getPopularVenues': 'https://api.foursquare.com/v2/venues/explore',
-//         'searchVenues': 'https://api.foursquare.com/v2/venues/search',
-//         'getTrendingVenues': 'https://api.foursquare.com/v2/venues/trending',
-    }
+//name, photo, rating, link
 
-    
-    var getVenueByLocation = function () {
-        axios.get(`${endPoints.getPopularVenues}?client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee`)
-            .then(function(data) {
-                console.log(data)
-                // Code for handling API response
-            })
-            .catch(function(error) {
-                console.log(error)
-                // Code for handling errors
+var getVenueByLocation = function () {
+    axios.get(`https://api.foursquare.com/v2/venues/explore?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323&ll=${currentLocation}&query=restaurant`)
+        .then(function(data) {
+            var informationPath = data.data.response.groups[0].items;
+            var fiveRestaurantIds = [];
+            for(i=0; i<5; i++) {
+                fiveRestaurantIds.push(informationPath[i].venue.id);
+            }
+            console.log(fiveRestaurantIds);
+            for(i=0;i<5; i++){
+                axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[i]+ `?client_id=YRYBPL3XX1AQMOAQF3EBE1Z2MWBGYWMH3F5U4XQKWWJ2ZUO3&client_secret=DAHBINRS4RAPF55R30CJUOXLHHRHS3XISOAI1C35BXAP4IIF&v=20180323`)
+                .then(function(data) {
+                    var newObject = {};
+                    console.log(data.data.response.venue);
+                    newObject.name = data.data.response.venue.name; 
+                    newObject.url = data.data.response.venue.url;
+                    newObject.rating = data.data.response.venue.rating;
+                    newObject.priceTier = data.data.response.venue.price.tier;
+                    newObject.photo = data.data.response.venue.bestPhoto.prefix + "/original/" + data.data.response.venue.bestPhoto.suffix;
+                    fiveRestaurants[i] = newObject;
+                })
+                .catch(function(error) {
+                    console.log(error)
+                     //Code for handling errors
+                });
+            }
+        })
+        .catch(function(error) {
+            console.log(error)
+            // Code for handling errors
         });
-    }
+}
 
+getVenueByLocation();
+
+exploreButton.onclick = function () {
+    //add below code when it is working to type in a location
+    if(locationSearchElement.value && locationSearch.value !== "" ){
+       currentLocation = locationSearchElement.value;
+    }
     getVenueByLocation();
+}
