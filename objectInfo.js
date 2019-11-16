94d940e74029b3a0c87b0dd2d5ae2cdea5b591e3
Barbie <barbara.kelly892@gmail.com>
2019-11-16 14:19:09 -0500

    Creating 5 restaurant cards

---
+++ objectInfo.js
@@ -0,0 +1,30 @@
+var fiveRestaurants = [
+    {
+        name: "Little Alley Steakhouse", 
+        url: "https://www.littlealleysteak.com/Buckhead/", 
+        rating: 8, 
+        priceTier: 4, 
+        photo: "https://fastly.4sqi.net/img/general/original/18276…5_dAtjOMo_qeFG42jp_pZTWG0ncqzUDiwaJw-jI0TdJ0Y.jpg"
+    },
+    {
+        name: "Maggiano's Little Italy", 
+        url: "https://locations.maggianos.com/georgia/atlanta/3368-peachtree-rd./", 
+        rating: 8.3, 
+        priceTier: 2, 
+        photo: "https://fastly.4sqi.net/img/general/original/7OPjb_taZ6jCE6tqIyBH7G517W32OnjbwF3c0T5zmfE.jpg"
+    },
+    {
+        name: "Del Frisco's Grille", 
+        url: "https://delfriscosgrille.com/atlanta", 
+        rating: 8.3, 
+        priceTier: 3, 
+        photo: "https://fastly.4sqi.net/img/general/original/24960…5_D9mn6noveydc-znB-rkkkPTPBm-tQOxGdj9R2IKcs0U.jpg"
+    },
+    {
+        name: "New York Prime Steakhouse", 
+        url: "http://www.newyorkprime.com", 
+        rating: 8.3, 
+        priceTier: 4, 
+        photo: "https://fastly.4sqi.net/img/general/original/VcCxheCoPBRt5p35vLkcbg4qQDSrUeCyzFazs8Sg9HE.jpg"
+    }
+];
--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 var exploreButton = document.getElementById("explore");
 const fourSquareClientId = "BCEJY3NTBHJNIUQF5DSI01IRXSDGJMTBBQPSPIK4OFTMCOUM";// Required
-const fourSquareClientSecret = "GGTZU3VGFAWCSA2H4HBL4A3HDJ1YTJWD3JAPMSE0BRLLXMDP";//Required
+const fourSquareClientSecret = "O1CAYJY0HSXZ45GATTKELQPTKAQXDWSI5KNW5A4MCEM2NPKF";//Required
 var locationSearchElement = document.getElementById("locationSearch");
-var container = document.getElementById("container");
+var cardContainer = document.getElementById("cardContainer");
 var currentLocation = "33.853510, -84.368667";
 var fiveRestaurants = [];
+var isFinished = 0;
 
-//name, photo, rating, link
+var createRestaurantCards = function(){
+    for (i=0; i<5; i++){
+        var newCardContainer = document.createElement("div");
+        newCardContainer.className ="card card_"+i+"_div";
+        var newCard = document.createElement("div");
+        newCard.className = "card";
+        newCard.style = "width: 25rem;";
+        var newCardImage = document.createElement("img");
+        newCardImage.className = "card-img-top";
+        newCardImage.src = fiveRestaurants[i].photo;
+        newCardImage.alt = "...";
+        var newCardBody = document.createElement("div");
+        newCardBody.className = "card-body";
+        var newCardTitle = document.createElement("h5");
+        newCardTitle.className = "card-title";
+        newCardTitle.innerHTML = fiveRestaurants[i].name;
+        var newCardInfo = document.createElement("p");
+        newCardInfo.className = "card-text";
+
+        var dollars = "";
+        if(fiveRestaurants[i].priceTier == 1){dollars = "$";}
+        if(fiveRestaurants[i].priceTier == 2){dollars = "$$";}
+        if(fiveRestaurants[i].priceTier == 3){dollars = "$$$";}
+        if(fiveRestaurants[i].priceTier == 4){dollars = "$$$$";}
+        if(fiveRestaurants[i].priceTier == 5){dollars = "$$$$$";}
+
+        newCardInfo.innerHTML = "rating: " + fiveRestaurants[i].rating + ", price: " + dollars;
+
+        newCardLink = document.createElement("a");
+        newCardLink.className = "card-link";
+        newCardLink.href = fiveRestaurants[1].url;
+        newCardLink.target = "_blank";
+        newCardLink.innerHTML = "take me there!";
+
+        
+        newCardBody.appendChild(newCardTitle);
+        newCardBody.appendChild(newCardInfo);
+        newCardBody.appendChild(newCardLink);
+        
+        newCard.appendChild(newCardImage);
+        newCard.appendChild(newCardBody);
+        newCardContainer.appendChild(newCard);
+        cardContainer.appendChild(newCardContainer);
+    }
+}
+
 
 var getVenueByLocation = function () {
     axios.get(`https://api.foursquare.com/v2/venues/explore?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323&ll=${currentLocation}&query=restaurant`)
@@ -16,36 +62,147 @@ var getVenueByLocation = function () {
             for(i=0; i<5; i++) {
                 fiveRestaurantIds.push(informationPath[i].venue.id);
             }
-                console.log(fiveRestaurantIds);
-            for(i=0;i<1; i++){
-                axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[i]+ `?client_id=YRYBPL3XX1AQMOAQF3EBE1Z2MWBGYWMH3F5U4XQKWWJ2ZUO3&client_secret=DAHBINRS4RAPF55R30CJUOXLHHRHS3XISOAI1C35BXAP4IIF&v=20180323`)
-                .then(function(data) {
-                    informationPath = data.data.response.venue;
-                    var newObject = {};
-                    newObject.name = informationPath.name; 
-                    newObject.url = data.data.response.venue.url;
-                    newObject.rating = data.data.response.venue.rating;
-                    newObject.priceTier = data.data.response.venue.price.tier;
-                    newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
-                    fiveRestaurants.push(newObject);
-                })
-                .catch(function(error) {
-                    console.log(error)
-                     //Code for handling errors
-                });
-            }
-                console.log(fiveRestaurants);
+            
+            axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[0]+ `?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323`)
+            .then(function(data) {
+                console.log("0");
+                informationPath = data.data.response.venue;
+                var newObject = {};
+                newObject.name = informationPath.name; 
+                newObject.url = data.data.response.venue.url;
+                newObject.rating = data.data.response.venue.rating;
+                newObject.priceTier = data.data.response.venue.price.tier;
+                newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
+                fiveRestaurants.push(newObject);
+                isFinished += 1;
+            })
+            .catch(function(error) {
+                console.log(error)
+                //Code for handling errors
+            });
+
+            axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[1]+ `?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323`)
+            .then(function(data) {
+                console.log("1");
+                informationPath = data.data.response.venue;
+                var newObject = {};
+                newObject.name = informationPath.name; 
+                newObject.url = data.data.response.venue.url;
+                newObject.rating = data.data.response.venue.rating;
+                newObject.priceTier = data.data.response.venue.price.tier;
+                newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
+                fiveRestaurants.push(newObject);
+                isFinished += 1;
+            })
+            .catch(function(error) {
+                console.log(error)
+                //Code for handling errors
+            });
+
+            axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[2]+ `?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323`)
+            .then(function(data) {
+                console.log("2");
+                informationPath = data.data.response.venue;
+                var newObject = {};
+                newObject.name = informationPath.name; 
+                newObject.url = data.data.response.venue.url;
+                newObject.rating = data.data.response.venue.rating;
+                newObject.priceTier = data.data.response.venue.price.tier;
+                newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
+                fiveRestaurants.push(newObject);
+                isFinished += 1;
+            })
+            .catch(function(error) {
+                console.log(error)
+                //Code for handling errors
+            });
+
+            axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[3]+ `?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323`)
+            .then(function(data) {
+                console.log("3");
+                informationPath = data.data.response.venue;
+                var newObject = {};
+                newObject.name = informationPath.name; 
+                newObject.url = data.data.response.venue.url;
+                newObject.rating = data.data.response.venue.rating;
+                newObject.priceTier = data.data.response.venue.price.tier;
+                newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
+                fiveRestaurants.push(newObject);
+                isFinished += 1;
+            })
+            .catch(function(error) {
+                console.log(error)
+                //Code for handling errors
+            });
+
+            axios.get(`https://api.foursquare.com/v2/venues/` + fiveRestaurantIds[4]+ `?client_id=${fourSquareClientId}&client_secret=${fourSquareClientSecret}&v=20180323`)
+            .then(function(data) {
+                console.log("4");
+                informationPath = data.data.response.venue;
+                var newObject = {};
+                newObject.name = informationPath.name; 
+                newObject.url = data.data.response.venue.url;
+                newObject.rating = data.data.response.venue.rating;
+                newObject.priceTier = data.data.response.venue.price.tier;
+                newObject.photo = data.data.response.venue.bestPhoto.prefix + "original" + data.data.response.venue.bestPhoto.suffix;
+                fiveRestaurants.push(newObject);
+                isFinished += 1;
+            })
+            .catch(function(error) {
+                console.log(error)
+                //Code for handling errors
+            });
+
+            console.log(fiveRestaurants);          
         })
+        
         .catch(function(error) {
             console.log(error)
             // Code for handling errors
         });
 }
 
+
 exploreButton.onclick = function () {
-    //add below code when it is working to type in a location
-    //if(locationSearchElement.value && locationSearch.value !== "" ){
+    // add below code when it is working to type in a location
+    // if(locationSearchElement.value && locationSearch.value !== "" ){
     //   currentLocation = locationSearchElement.value;
-    //}
+    // }
+    cardContainer.innerHTML = "";
     getVenueByLocation();
+    setTimeout(function(){ 
+        createRestaurantCards();
+    }, 3000);
 }
+
+$(document).ready(function() {
+    $(".card_1").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".card_1_div").offset().top},
+            'slow');
+    });
+
+    $(".card_2").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".card_2_div").offset().top},
+            'slow');
+    });
+
+    $(".card_3").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".card_3_div").offset().top},
+            'slow');
+    });
+
+    $(".card_4").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".card_4_div").offset().top},
+            'slow');
+    });
+
+    $(".card_5").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".card_5_div").offset().top},
+            'slow');
+    });
+})
